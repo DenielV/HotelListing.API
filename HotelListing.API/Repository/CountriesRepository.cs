1731ac3abb998672dc5e702dff6ae0698beb4280
dvillare <denielfelix123@gmail.com>
2023-07-13 20:51:28 -0700

    REST API Development Best Practices. Automapper, DTOs and Repositories

---
+++ HotelListing.API/Repository/CountriesRepository.cs
@@ -0,0 +1,20 @@
+﻿using HotelListing.API.Contracts;
+using HotelListing.API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace HotelListing.API.Repository
+{
+    public class CountriesRepository : GenericRepository<Country>, ICountriesRepository
+    {
+        private readonly HotelListingDbContext _dbContext;
+        public CountriesRepository(HotelListingDbContext dbContext) : base(dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<Country> GetDetailsAsync(int id)
+        {
+            return await _dbContext.Countries.Include(c => c.Hotels).FirstOrDefaultAsync(c => c.Id == id);
+        }
+    }
+}
